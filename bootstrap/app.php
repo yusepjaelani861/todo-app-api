af29b8d35d8af04d427e1ba29713af838cba97e3
yusepjaelani861 <yusepjaelani861@gmail.com>
2024-03-21 14:07:19 +0700

    remove csrf

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,10 @@ return Application::configure(basePath: dirname(__DIR__))
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
         ]);
 
+        $middleware->validateCsrfTokens(except: [
+            '/api/*',
+        ]);
+
         $middleware->alias([
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
         ]);
